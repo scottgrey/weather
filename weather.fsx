4a63dc3de99453bd25555da0be773ec26baae0ca
Scott Grey <scottgrey@gmail.com>
2017-02-02 09:46:21 -0600

    adding influxdb and made to run continuously

--- weather.fsx
+++ weather.fsx
@@ -70,3 +70,118 @@ for dc in d do
     let m = dc.Modules
     for md in m do
         printfn "%s" md.ModuleName
+
+(*printfn "Device: %s" stationName
+        printfn ""
+        printfn "Indoor measurement time(UTC): %s" (toDateTime(timeUtc).ToString())
+        printfn "Indoor measurement time(Local): %s" (toDateTimeLocal(timeUtc).ToString())
+        printfn "Indoor last status store time(UTC): %s" (toDateTime(lastStatusStore).ToString())
+        printfn "Indoor last status store time(Local): %s" (toDateTimeLocal(lastStatusStore).ToString())
+        printfn "Indoor setup time(UTC): %s" (toDateTime(dateSetup).ToString())
+        printfn "Indoor setup time(Local): %s" (toDateTimeLocal(dateSetup).ToString())
+        printfn "Indoor last setup time(UTC): %s" (toDateTime(lastSetup).ToString())
+        printfn "Indoor last setup time(Local): %s" (toDateTimeLocal(lastSetup).ToString())
+        printfn "Indoor last upgrade time(UTC): %s" (toDateTime(lastUpgrade).ToString())
+        printfn "Indoor last upgrade time(Local): %s" (toDateTimeLocal(lastUpgrade).ToString())
+        printfn "CO2 Calibrating: %b" dc.Co2Calibrating
+        printfn "Indoor wifi status: %i" dc.WifiStatus
+        printfn "Indoor firmware: %i" dc.Firmware
+        printfn "Indoor Temp: %.2f" (ctof (float(dc.DashboardData.Temperature)))
+        printfn "Indoor Temp trend: %s" dc.DashboardData.TempTrend
+        printfn "Indoor Humidity: %i" dc.DashboardData.Humidity
+        printfn "Pressure: %M" dc.DashboardData.Pressure
+        printfn "Pressure trend: %s" dc.DashboardData.PressureTrend
+        printfn "Absolute Presssure: %M" dc.DashboardData.AbsolutePressure
+        printfn "Noise: %i" dc.DashboardData.Noise
+        printfn "CO2: %i" dc.DashboardData.Co2
+        printfn "Max Indoor Temp: %.2f" (ctof (float(dc.DashboardData.MaxTemp)))
+        printfn "DateTime of Max Indoor Temp (UTC): %s" (toDateTime(dc.DashboardData.DateMaxTemp).ToString())
+        printfn "DateTime of Max Indoor Temp (Local): %s" (toDateTimeLocal(dc.DashboardData.DateMaxTemp).ToString())
+        printfn "Min Indoor Temp: %.2f" (ctof (float(dc.DashboardData.MinTemp)))
+        printfn "DateTime of Min Indoor Temp (UTC): %s" (toDateTime(dc.DashboardData.DateMinTemp).ToString())
+        printfn "DateTime of Min Indoor Temp (Local): %s" (toDateTimeLocal(dc.DashboardData.DateMinTemp).ToString())
+*)
+        //for m in dc.Modules do
+        //    let mn = m.ModuleName
+           (* printfn ""
+            printfn "Module Name: %s" mn
+            printfn "%s Battery Status: %i" mn m.BatteryPercent
+            printfn "%s Battery volt: %i" mn m.BatteryVp
+            printfn "%s measurement time(UTC): %s" mn (toDateTime(m.DashboardData.TimeUtc).ToString())
+            printfn "%s measurement time(Local): %s" mn (toDateTimeLocal(m.DashboardData.TimeUtc).ToString())
+            printfn "%s last message time(UTC): %s" mn (toDateTime(m.LastMessage).ToString())
+            printfn "%s last message time(Local): %s" mn (toDateTimeLocal(m.LastMessage).ToString())
+            printfn "%s last seen time(UTC): %s" mn (toDateTime(m.LastSeen).ToString())
+            printfn "%s last seen time(Local): %s" mn (toDateTimeLocal(m.LastSeen).ToString())
+            printfn "%s last setup time(UTC): %s" mn (toDateTime(m.LastSetup).ToString())
+            printfn "%s last setup time(Local): %s" mn (toDateTimeLocal(m.LastSetup).ToString())
+            printfn "%s rf status: %i" mn m.RfStatus
+            printfn "%s firmware: %i" mn m.Firmware
+            match mn with
+                | "Outdoor" -> 
+                    let ot = m.DashboardData.Temperature
+                        
+                    match ot with
+                    | Some t -> 
+                        printfn "%s Temp: %.2f" mn (ctof (float(t)))
+                    | None -> ()
+                    
+                    let tt = m.DashboardData.TempTrend
+                        
+                    match tt with
+                    | Some t -> printfn "%s Temp trend: %s" mn t
+                    | None -> ()
+
+                    let hy = m.DashboardData.Humidity
+
+                    match hy with
+                    | Some h -> printfn "%s Humidity: %i" mn h
+                    | None -> ()
+
+                    let dmaxt = m.DashboardData.DateMaxTemp
+                    let maxt = m.DashboardData.MaxTemp
+
+                    match maxt with
+                    | Some t -> printfn "Max %s Temp: %.2f" mn (ctof (float(t)))
+                    | None -> ()
+
+                    match dmaxt with
+                    | Some t -> 
+                        printfn "DateTime of Max %s Temp (UTC): %s" mn (toDateTime(t).ToString())
+                        printfn "DateTime of Max %s Temp (Local): %s" mn (toDateTimeLocal(t).ToString())
+                    | None -> ()
+
+                    let dmint = m.DashboardData.DateMinTemp
+                    let mint = m.DashboardData.MinTemp
+
+                    match mint with
+                    | Some t -> printfn "Min %s Temp: %.2f" mn (ctof (float(t)))
+                    | None -> ()
+
+                    match dmint with
+                    | Some t -> 
+                        printfn "DateTime of Min %s Temp (UTC): %s" mn (toDateTime(t).ToString())
+                        printfn "DateTime of Min %s Temp (Local): %s" mn (toDateTimeLocal(t).ToString())
+                    | None -> ()
+
+                | "Rain" -> 
+                    let rn = m.DashboardData.Rain
+
+                    match rn with
+                    | Some r -> printfn "%s since last measurement: %.4f" mn (mmtoin (float(r)))
+                    | None -> ()
+                        
+                    let rn1 = m.DashboardData.SumRain1
+
+                    match rn1 with
+                    | Some r -> printfn "%s in last hour: %.4f" mn (mmtoin (float(r)))
+                    | None -> ()
+
+                    let rn24 = m.DashboardData.SumRain24
+
+                    match rn24 with
+                    | Some r -> printfn "%s in last 24 hours: %.4f" mn (mmtoin (float(r)))
+                    | None -> ()
+
+                | _ -> ()
+            printfn ""*)
